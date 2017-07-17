97f0a32b40a8fc96b7d3dac812f11435e0565fa2
Parent Jerome <jerome.parent@lynceetec.com>
2017-07-17 15:34:01 +0200

    Upload Maven Project

---
+++ src/main/java/Get_Bin_Info.java
@@ -0,0 +1,13 @@
+
+import ij.*;
+import ij.io.*;
+import ij.plugin.*;
+
+public class Get_Bin_Info implements PlugIn {
+    public void run(String arg) {
+    	ImagePlus img = IJ.getImage(); 
+    	FileInfo fi = img.getOriginalFileInfo();
+    	IJ.showMessage(fi.debugInfo);
+    	return;
+    }
+}
